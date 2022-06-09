a6a17abbcf9c1414dd6dc92c28d3b957c1a01b7e
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-06-09 10:22:56 -0500

    card component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CardModule } from 'projects/card/src/public-api';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
